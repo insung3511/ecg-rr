fa675de81d50f928d3798ca9ffd0213facd0335d
insung3511 <insung.park123@gmail.com>
2022-05-24 21:44:52 +0900

    Success selecting rtor

--- peak_sel.py
+++ peak_sel.py
@@ -1,7 +1,8 @@
+from os import pread
 import matplotlib.pyplot as plt
 import wfdb
 
-R_PATH = "./data/mit/"
+R_PATH = "./data/nstdb/"
 exclude_record = ["bw", "em", "ma"]
 
 zero_padded_list = []
@@ -25,12 +26,20 @@ for i in range(len(record_list)):
     temp_rpath = R_PATH + record_list[i]
 
     # Read original sample by rdsamp function
-    record_sg, _ = wfdb.rdsamp(temp_rpath, channels=[0], sampfrom=0)
+    record_sg, _ = wfdb.rdsamp(temp_rpath, channels=[1], sampfrom=0)
 
     # Got R-R Peak by rdann funciton
     record_ann = list(wfdb.rdann(temp_rpath, 'atr', sampfrom=0).sample)[1:]
 
     for i in range(len(record_ann)):
-        print(record_ann[i], record_ann[i+1])
-        plt.plot(record_sg[record_ann[i]:record_ann[i+1]])
+        pre_add = record_ann[i - 1]
+        post_add = record_ann[i + 1]
+        print(pre_add, post_add)
+
+        if i < 1:
+            pre_add = 0
+
+        plt.title(temp_rpath)
+        plt.grid(color='0.5')
+        plt.plot(record_sg[pre_add:post_add])
         plt.show()
