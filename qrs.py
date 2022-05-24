93f231379ab866db4d64a0bb666e49a5aa5ac3be
insung3511 <insung.park123@gmail.com>
2022-05-24 09:59:18 +0900

    Add more databases

--- qrs.py
+++ qrs.py
@@ -1,21 +1,43 @@
 from ast import AnnAssign
+from re import L
 import matplotlib.pyplot as plt
 import wfdb.processing as wp
 import wfdb
 
-sig, fields = wfdb.rdsamp('data/mit/100')
-rrin = wp.ann2rr('data/mit/100', 'atr', as_array=True, start_time=0.0)
-xqrs = wp.XQRS(sig=sig[:, 0], fs=fields['fs'])
-xqrs.detect()
+PATH = 'data/nstdb/'
+exclude_record = ['bw', 'em', 'ma']
+record_list = []
+
+print("[INFO] Read records file from ", PATH)
+with open(PATH + 'RECORDS') as f:
+    record_lines = f.readlines()
+
+# Read Records
+for i in range(len(record_lines)):
+    record_list.append(str(record_lines[i].strip()))
+for i in range(len(record_lines)):
+    record_lines[i] = record_lines[i].strip()
+
+print(record_list)
+
+for i in range(len(record_lines)):
+    r_path = PATH + record_lines[i]
+    sig, fields = wfdb.rdsamp(r_path)
+    rrin = wp.ann2rr(r_path, 'atr', as_array=True, start_time=0.0)
+    xqrs = wp.XQRS(sig=sig[:, 0], fs=fields['fs'])
+    
+    xqrs.detect()
+    print(sig, rrin)
 
 pre_rrin_temp = 0
 post_rrin_temp = 0
-for i in range(len(sig)):
-    post_rrin_temp += rrin[i]
 
-    plt.plot(sig[pre_rrin_temp : post_rrin_temp])
-    plt.title(i)
-    plt.show()
+# for i in range(len(sig)):
+#     post_rrin_temp += rrin[i]
 
-    pre_rrin_temp += post_rrin_temp
-    print(pre_rrin_temp, post_rrin_temp, rrin[i])
+#     plt.plot(sig[pre_rrin_temp : post_rrin_temp])
+#     plt.title(i)
+#     plt.show()
+
+#     pre_rrin_temp += post_rrin_temp
+#     print(pre_rrin_temp, post_rrin_temp, rrin[i])
