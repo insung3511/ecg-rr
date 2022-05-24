f1532e0154183f7f70515f9835f40ac0b48bcd88
insung3511 <insung.park123@gmail.com>
2022-05-24 23:25:08 +0900

    MIT, NST Zero Padding success

--- peak_sel.py
+++ peak_sel.py
@@ -1,8 +1,10 @@
-import matplotlib.pyplot as plt
 import wfdb.processing as wp
+import numpy as np
+import pickle
 import wfdb
 
-R_PATH = "./data/nstdb/"
+R_PATH = "./data/cudb/"
+DB_NAME = "cudb"
 exclude_record = ["bw", "em", "ma"]
 
 zero_padded_list = []
@@ -10,6 +12,10 @@ windowed_list = []
 record_list = []
 record_ann = []
 longest = 0.
+
+def flatter(list_of_list):
+    flatList = [ item for elem in list_of_list for item in elem]
+    return flatList
 
 # Read RECORDS txt file
 print("[INFO] Read records file from ", R_PATH)
@@ -29,8 +35,11 @@ for i in range(len(record_list)):
     longest = interval.max()
 print("LONGEST: ", longest)
 
+
+
 for i in range(len(record_list)):
     temp_rpath = R_PATH + record_list[i]
+    temp_pickle = "./pickle/" + DB_NAME + record_list[i] + ".pkl"
 
     # Read original sample by rdsamp function
     record_sg, _ = wfdb.rdsamp(temp_rpath, channels=[0], sampfrom=0)
@@ -39,21 +48,32 @@ for i in range(len(record_list)):
     record_ann = list(wfdb.rdann(temp_rpath, 'atr', sampfrom=0).sample)[1:]
 
     for i in range(len(record_ann)):
-        pre_add = record_ann[i - 1]
-        post_add = record_ann[i + 1]
+        try:
+            pre_add = record_ann[i - 1]
+            post_add = record_ann[i + 1]
+        except IndexError:
+            break
 
         cut_pre_add = record_ann[i] - int((record_ann[i] - pre_add) / 2) 
         cut_post_add = record_ann[i] + int((post_add - record_ann[i]) / 2)
 
-        print(pre_add, post_add, "\t\t\t", cut_pre_add, cut_post_add)
+        # print(pre_add, post_add, "\t\t\t", cut_pre_add, cut_post_add)
 
         if i < 1:
-            pre_add = 0
+            continue
         
-        windowed_list.append(record_sg[pre_add:post_add])
+        windowed_list = flatter(record_sg[cut_pre_add:cut_post_add])
+        # print(windowed_list)
 
-        plt.title(temp_rpath)
-        plt.grid(color='0.5')
-        plt.plot(record_sg[cut_pre_add:cut_post_add])
-        plt.plot(record_sg[pre_add:post_add])
-        plt.show()
+        zero_padded_list = (
+            (
+                np.pad( windowed_list, 
+                        int(longest - len(windowed_list) / 4), 
+                        'constant', 
+                        constant_values=0)
+            ).tolist()
+        )
+
+        with open(temp_pickle, "wb") as f:
+            pickle.dump(zero_padded_list, f)
+    print(temp_pickle, " SAVED!")
