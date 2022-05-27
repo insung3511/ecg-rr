dd6ec0e3e2df70a7a8341ca6c27b989dd91a690e
insung3511 <insung.park123@gmail.com>
2022-05-27 15:18:02 +0900

    Change annotation rule

--- README.md
+++ README.md
@@ -2,4 +2,46 @@
 R-R Interval 을 기점으로 비트를 잘라내는 프로젝트이다. 이론은 간단한데 코드로 작성하자니 어렵다.
 
 ## Logic
-A Record가 있다고 하자. A 레코드는 길이가 3시간 분량이고 R-R Interval 길이가 다양하다고 가정하면 여기서 평균을 구한다. 각 평균을 구해 평균에 맞게 갖고오는데 만일 비트가 짧거나 맞지 않는다면 Zero-padding을 해주는 것이 이번 프로젝트(?) 의 목표이다.
+여러 레코드마다 각각 비트 길이가 있고 그에 따른 비트 길이 또한 다르다. 대부분 논문에서는 비트를 뽑아 재가공 혹은 모델의 입맛에 맞게 준다. 논문에서는 어떻게 했다고는 하지만 그런 내용은 한 달락이면 끝이다. 무튼, 얘기가 길어졌는데 이 프로젝트의 주 목적은 **각 레코드를 같은 사이즈의 윈도우로 끊어 갖고 오는 것이다.**
+
+아마 pickle 디렉토리에 다 담긴걸 보면 일종의 스크랩 북이 될 것이다... ~~여러 비트를 자르고 잘라서 하나의 폴더 혹은 파일이 짜잔~~
+
+### Find R Peak
+가장 먼저 R peak를 찾아야 한다. R peak로 보통 annotation이며 거의 원점 처럼 여겨지기에 R Peak를 찾고 이를 기점으로 자르는 것이 중요하다. R peak를 찾는 건 어렵지 않다.
+
+```python
+for i in range(len(record_list)):
+    temp_rpath = R_PATH + record_list[i]
+    interval = wp.ann2rr(temp_rpath, 'atr', as_array=True)
+    longest = interval.max()
+print("LONGEST: ", longest)
+```
+
+record_list는 읽어올 record의 파일명, temp_rpath는 지속적으로 변경될 path를 지정해준다. 정해진 path로 wp (wfdb.processing module) 의 ann2rr 를 통해 R Peak의 길이를 갖고온다. 그 중에서 가장 긴 길이를 찾아내어 longest로 지정. 
+
+### Cutting it
+
+```python
+for i in range(len(record_ann)):
+    try:
+        pre_add = record_ann[i - 1]
+        post_add = record_ann[i + 1]
+
+    except IndexError:
+        break
+
+    cut_pre_add = record_ann[i] - int((record_ann[i] - pre_add) / 2) 
+    cut_post_add = record_ann[i] + int((post_add - record_ann[i]) / 2
+    
+    if i < 1:
+        continue
+    
+    windowed_list = flatter(record_sg[cut_pre_add:cut_post_add]
+    zero_padded_list.append(np.pad(windowed_list, int(longest/ 2), 'constant', constant_values=0))
+    dict_ann.append(record_ann_sym[i]
+    
+    if record_ann_sym[i] != "N":
+        print("Abnormal!", temp_rpath, "\t|\t", record_ann_sym[i]
+```
+
+이제 비트에서 잘라내는 과정이다. pre_add, post_add는 current 비트 기준 앞 뒤, 비트이고 cut_pre_add와 cut_post_add 는 이제 자를 비트의 위치이다.
--- peak_sel.py
+++ peak_sel.py
@@ -1,11 +1,21 @@
+from tabnanny import check
+from attr import field
 import matplotlib.pyplot as plt
 import wfdb.processing as wp
 import numpy as np
 import pickle
 import wfdb
 
+PICKLE_PATH = "./pickle_May27_2022_15_16/"
 DEFAULT_PATH = "./data/"
-DB_PATH = ['mit', 'nstdb', 'esc', 'cudb']
+DB_PATH = ['mit', 'nstdb']
+EXTRA_NP = np.array(0)
+
+NORAML_ANN = ['N', 'L', 'R', 'e', 'j']
+SUPRA_ANN = ['A', 'a', 'J', 'S']
+VENTRI_ANN = ['V', 'E']
+FUSION_ANN = ['F']
+UNCLASS_ANN = ['/', 'f', 'Q']
 
 for k in range(len(DB_PATH)):
     R_PATH = DEFAULT_PATH + DB_PATH[k] + "/"
@@ -34,16 +44,11 @@ for k in range(len(DB_PATH)):
             continue
         record_list.append(str(record_lines[i].strip()))
 
-    # Find Longest R-R Interval for window
-    for i in range(len(record_list)):
-        temp_rpath = R_PATH + record_list[i]
-        interval = wp.ann2rr(temp_rpath, 'atr', as_array=True)
-        longest = interval.max()
     print("LONGEST: ", longest)
 
     for j in range(len(record_list)):
         temp_rpath = R_PATH + record_list[j]
-        temp_pickle = "./pickle/" + DB_PATH[k] + record_list[j] + ".pkl"
+        temp_pickle = PICKLE_PATH + DB_PATH[k] + record_list[j] + ".pkl"
 
         # Read original sample by rdsamp function
         record_sg, _ = wfdb.rdsamp(temp_rpath, channels=[0], sampfrom=0)
@@ -52,6 +57,8 @@ for k in range(len(DB_PATH)):
         record_ann = list(wfdb.rdann(temp_rpath, 'atr', sampfrom=0).sample)[1:]
         record_ann_sym = list(wfdb.rdann(temp_rpath, 'atr', sampfrom=0).symbol)[1:]
 
+        interval = wp.ann2rr(temp_rpath, 'atr', as_array=True)
+        
         for i in range(len(record_ann)):
             try:
                 pre_add = record_ann[i - 1]
@@ -59,6 +66,7 @@ for k in range(len(DB_PATH)):
             except IndexError:
                 break
 
+            avg_div = (interval[i - 1] + interval[i]) / 2
             cut_pre_add = record_ann[i] - int((record_ann[i] - pre_add) / 2) 
             cut_post_add = record_ann[i] + int((post_add - record_ann[i]) / 2)
 
@@ -66,16 +74,33 @@ for k in range(len(DB_PATH)):
                 continue
             
             windowed_list = flatter(record_sg[cut_pre_add:cut_post_add])
+            if len(windowed_list) > 428:
+                cut_it_off = 0
+            else:
+                cut_it_off = int((428 - len(windowed_list)) / 2)
 
-            zero_padded_list.append(np.pad(windowed_list, int(longest/ 2), 'constant', constant_values=0))
-            dict_ann.append(record_ann_sym[i])
-
-            if record_ann_sym[i] != "N":
-                print("Abnormal!", temp_rpath, "\t|\t", record_ann_sym[i])
-
+            if len(np.pad(windowed_list, cut_it_off, 'constant', constant_values=0)) == 427:
+                zero_padded_list.append(np.append([0.0], np.pad(windowed_list, cut_it_off , 'constant', constant_values=0)))
+            
+            check_ann = record_ann_sym[i]
+            if check_ann in NORAML_ANN:
+                record_ann_sym[i] = "N"
+            elif check_ann in SUPRA_ANN:
+                record_ann_sym[i] = "S"
+            elif check_ann in VENTRI_ANN:
+                record_ann_sym[i] = "V"
+            elif check_ann in FUSION_ANN:
+                record_ann_sym[i] = "F"
+            elif check_ann in UNCLASS_ANN:
+                record_ann_sym[i] = "Q"
+            else:
+                record_ann_sym[i] = "?"
+            
+            # plt.plot(np.pad(windowed_list, cut_it_off, 'constant', constant_values=0))
             # plt.title(record_ann_sym[i])
-            # plt.plot(np.pad(windowed_list, int(longest / 2), 'constant', constant_values=0))
             # plt.show()
+
+            dict_ann.append(record_ann_sym[i])
 
         ann_dict = {
             0 : zero_padded_list,
